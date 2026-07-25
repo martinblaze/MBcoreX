e21dcd256e82d6fa3759cd140277691a231e1768
Martin <martinchidozie27@gmail.com>
2026-07-25 01:16:05 +0100

    favicon

--- components/layout/mobile-nav.tsx
+++ components/layout/mobile-nav.tsx
@@ -33,7 +33,7 @@ export function MobileNav() {
       <SheetContent side="right" className="w-full sm:max-w-sm">
         <SheetHeader className="border-b border-border">
           <SheetTitle>
-            <Logo height={24} />
+            <Logo height={32} />
           </SheetTitle>
         </SheetHeader>
 
